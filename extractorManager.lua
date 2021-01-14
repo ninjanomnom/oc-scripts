11bee359ffee195a0eb5914bd4ed7b7b47fa0023
Emmett Gaines <ninjanomnom@gmail.com>
2021-01-14 15:38:37 -0500

    Makes extractor manager work continuously

--- extractorManager.lua
+++ extractorManager.lua
@@ -1,53 +1,52 @@
-local component = require("component")
-local sides = require("sides")
+local thread = require("thread")
 
-local args = {...}
-
-local extractor = component.extractor
-
-local a, b, stageOneInput, d, e = extractor.getSlot(0)
-if(stageOneInput == nil) then
-    stageOneInput = 0
+function ItemsInSlot(slot)
+    local component = require("component")
+    local extractor = component.extractor
+    
+    local a, b, count, d, e = extractor.getSlot(slot)
+    if(count == nil) then
+        count = 0
+    end
+    return count
 end
 
-local a, b, stageTwoInput, d, e = extractor.getSlot(1)
-if(stageTwoInput == nil) then
-    stageTwoInput = 0
+function Tick()
+    local component = require("component")
+    local redstone = component.redstone
+
+    local sides = require("sides")
+
+    local chosenSlot
+    local chosenStackCount = 0
+    for i = 0, 4 do
+        local stackCount = ItemsInSlot(i)
+        if(stackCount > chosenStackCount) then
+            chosenSlot = i
+            chosenStackCount = stackCount
+        end
+    end
+
+    if(chosenStackCount == 0) then
+        redstone.setOutput(sides.top, 0)
+        print("No inputs detected, disengaging.")
+        return
+    end
+
+    local stage = chosenSlot + 1
+
+    print("Activating stage " .. stage .. " with stack of " .. chosenStackCount .. " input items.")
+
+    redstone.setOutput(sides.top, stage)
 end
 
-local a, b, stageThreeInput, d, e = extractor.getSlot(2)
-if(stageThreeInput == nil) then
-    stageThreeInput = 0
-end
+local worker = thread.create(
+    function()
+        while(true) do
+            Tick()
+            os.sleep(10)
+        end
+    end
+)
 
-local a, b, stageFourInput, d, e = extractor.getSlot(3)
-if(stageFourInput == nil) then
-    stageFourInput = 0
-end
-
-local largestStackCount = math.max(stageOneInput, stageTwoInput, stageThreeInput, stageFourInput)
-
-if(largestStackCount == 0) then
-    redstone.setOutput(sides.top, 0)
-    print("No inputs detected, disengaging.")
-    return
-end
-
-local redstone = component.redstone
-local chosenOutput = 0
-
-if(stageOneInput == largestStackCount) then
-    chosenOutput = 1
-end
-if(stageTwoInput == largestStackCount) then
-    chosenOutput = 2
-end
-if(stageThreeInput == largestStackCount) then
-    chosenOutput = 3
-end
-if(stageFourInput == largestStackCount) then
-    chosenOutput = 4
-end
-
-print("Activating stage " .. chosenOutput .. " with stack of " .. largestStackCount .. " input items.")
-redstone.setOutput(sides.top, chosenOutput)
+worker:detach()
